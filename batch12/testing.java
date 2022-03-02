683dd3b6efa96af4f7a895300a66240cc0a4d927
Muhammad Shafiq <fayeq.shafiq@gmail.com>
2022-03-02 13:26:55 +0430

    batch12/

---
+++ batch12/testing.java
@@ -0,0 +1,15 @@
+package batch12;
+
+public class testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this is a code");
+
+
+
+		
+	}
+
+}
